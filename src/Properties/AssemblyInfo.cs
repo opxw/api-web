2a7c168bf40d1a354f81fa6978f3f633b6d98dd9
opx <pleasureunderpressure@gmail.com>
2026-07-13 16:29:54 +0700

    1.0.7

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyFileVersion("1.0.6.20260713")]
+[assembly: AssemblyFileVersion("1.0.7.20260713")]
 [assembly: InternalsVisibleTo("Opx.Api.Web.Tests")]
